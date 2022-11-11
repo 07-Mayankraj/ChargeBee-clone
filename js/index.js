59a41e664930d9f8814359f94b456dc68275a261
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-11-11 19:55:38 +0530

    added brand slider

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ let imgArr = [
         imga_name: 'Louiset,CFO Yousign',
         paratext: 'The revenue engine that powered MakeSpace to launch a B2B model overnight.'
     },
-    
+
     {
         image: 'https://webstatic.chargebee.com/assets/web/535/images/home/coc/customers/paul-kapsner.webp',
         imga_name: 'debbie barfellow,CEO marketplace',
@@ -32,59 +32,59 @@ let imgArr = [
     },
 ]
 
-let carouselArr = [
- 
-    "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-    "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-    "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-    "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-    "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-"https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+// let carouselArr = [
 
-]
-let j =0;
-let carousel = document.querySelector('#testimonial-brands')
-let imgnew = document.createElement('img')
-    imgnew.setAttribute('src',carouselArr[0])
-    let imgnew2 = document.createElement('img')
-    imgnew2.setAttribute('src',carouselArr[3])
-    let imgnew3 = document.createElement('img')
-    imgnew3.setAttribute('src',carouselArr[2])
-    carousel.append(imgnew,imgnew2,imgnew3)
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
+//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
 
-    setInterval(()=>{
-        if(j===carouselArr.length) j=0;
-        let carousel = document.querySelector('#testimonial-brands')
-        let imgggtransition =document.querySelector('#testimonial-brands>img')
-        let imgnew = document.createElement('img')
-        imgnew.className = 'brandcarousoltimedealay';
-    imgnew.setAttribute('src',carouselArr[j])
-    carousel.append(imgnew)
-    
-        // carousel.setAttribute('src',carouselArr[j])
-      
-            j++
+// ]
+// let j = 0;
+// let carousel = document.querySelector('#testimonial-brands')
+// let imgnew = document.createElement('img')
+// imgnew.setAttribute('src', carouselArr[0])
+// let imgnew2 = document.createElement('img')
+// imgnew2.setAttribute('src', carouselArr[3])
+// let imgnew3 = document.createElement('img')
+// imgnew3.setAttribute('src', carouselArr[2])
+// carousel.append(imgnew, imgnew2, imgnew3)
 
-},600)
+// setInterval(() => {
+//     if (j === carouselArr.length) j = 0;
+//     let carousel = document.querySelector('#testimonial-brands')
+//     let imgggtransition = document.querySelector('#testimonial-brands>img')
+//     let imgnew = document.createElement('img')
+//     imgnew.className = 'brandcarousoltimedealay';
+//     imgnew.setAttribute('src', carouselArr[j])
+//     carousel.append(imgnew)
+
+//     // carousel.setAttribute('src',carouselArr[j])
+
+//     j++
+
+// }, 600)
 
 let i = 0
 setInterval(() => {
 
-   
+
     let imgtag = document.querySelector('.image-slider>img')
     let ptag = document.querySelector('.namesofslider')
     let txt = document.querySelector('.section-1-heading>h1')
@@ -93,12 +93,12 @@ setInterval(() => {
     ptag.innerText = imgArr[i].imga_name;
     // console.log(imgArr[i].image);
     // console.log(imgArr[i].paratext);
-    let x = 'labelcustom'+i
+    let x = 'labelcustom' + i
     let slidebtn = document.getElementById(x)
     slidebtn.classList.add('sliderlabel')
-    setTimeout(()=>{
+    setTimeout(() => {
         slidebtn.classList.remove('sliderlabel')
-    },2000)
+    }, 2000)
 
     i++;
 
