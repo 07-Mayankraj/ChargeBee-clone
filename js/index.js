9836527ee369d9ea8f525611bc05f39942e6c3af
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-11-11 19:56:34 +0530

    finale commit

--- js/index.js
+++ js/index.js
@@ -32,54 +32,6 @@ let imgArr = [
     },
 ]
 
-// let carouselArr = [
-
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/calendly.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/okta.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-//     "https://webstatic.chargebee.com/assets/web/535/images/footer/getaccept.svg",
-
-// ]
-// let j = 0;
-// let carousel = document.querySelector('#testimonial-brands')
-// let imgnew = document.createElement('img')
-// imgnew.setAttribute('src', carouselArr[0])
-// let imgnew2 = document.createElement('img')
-// imgnew2.setAttribute('src', carouselArr[3])
-// let imgnew3 = document.createElement('img')
-// imgnew3.setAttribute('src', carouselArr[2])
-// carousel.append(imgnew, imgnew2, imgnew3)
-
-// setInterval(() => {
-//     if (j === carouselArr.length) j = 0;
-//     let carousel = document.querySelector('#testimonial-brands')
-//     let imgggtransition = document.querySelector('#testimonial-brands>img')
-//     let imgnew = document.createElement('img')
-//     imgnew.className = 'brandcarousoltimedealay';
-//     imgnew.setAttribute('src', carouselArr[j])
-//     carousel.append(imgnew)
-
-//     // carousel.setAttribute('src',carouselArr[j])
-
-//     j++
-
-// }, 600)
 
 let i = 0
 setInterval(() => {
